d5dc4855d7589b940f2dabacc8823603398680e9
Puneet Goel <puneetgoel016@gmail.com>
2025-01-05 22:09:32 +0530

    graphql query

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using GraphQL.Schema;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
+
+app.Run();
---
+++ Schema/CourseType.cs
@@ -0,0 +1,24 @@
+﻿namespace GraphQL.Schema
+{
+    public enum Subject
+    {
+        Maths,
+        Science,
+        English
+    }
+
+    public class CourseType
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Subject Subject { get; set; }
+        public IEnumerable<StudentType> Students { get; set; }
+        [GraphQLNonNullType]
+        public InstructorType Instructor { get; set; }
+
+        public string Description()
+        {
+            return "This is description resolver";
+        }
+    }
+}
